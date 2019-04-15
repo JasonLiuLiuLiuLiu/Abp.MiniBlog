7813c93b270ad950e88d7909eecac1c9cd992b72
Zhenyu,Liu <liuzhenyulive@live.com>
2019-04-15 23:33:06 +0800

    Add model method

--- Abp.MiniBlog/src/Abp.MiniBlog.Core/Comment/Comment.cs
+++ Abp.MiniBlog/src/Abp.MiniBlog.Core/Comment/Comment.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -24,6 +23,29 @@ namespace Abp.MiniBlog.Comment
         public virtual Blog.Blog  Blog{ get; protected set; }
 
         public virtual Guid BlogId { get; protected set; }
+
+        public string GetGravatar()
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(Email.Trim().ToLowerInvariant());
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                var sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+
+                return $"https://www.gravatar.com/avatar/{sb.ToString().ToLowerInvariant()}?s=60&d=blank";
+            }
+        }
+
+        public string RenderContent()
+        {
+            return Content;
+        }
 
     }
 }

167bd29b74d9971e2e0707f77e64c46eb8512838
Zhenyu,Liu <liuzhenyulive@live.com>
2019-04-17 01:04:26 +0800

    Add blogs

--- Abp.MiniBlog/src/Abp.MiniBlog.Core/Authorization/MiniBlogAuthorizationProvider.cs
+++ Abp.MiniBlog/src/Abp.MiniBlog.Core/Authorization/MiniBlogAuthorizationProvider.cs
@@ -10,6 +10,7 @@ namespace Abp.MiniBlog.Authorization
         {
             context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
             context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
+            context.CreatePermission(PermissionNames.Pages_Blogs, L("Blogs"));
             context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
         }
 
---
+++ Abp.MiniBlog/src/Abp.MiniBlog.Web.Mvc/Controllers/BlogsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
+using Abp.MiniBlog.Authorization;
+using Abp.MiniBlog.Blog;
+using Abp.MiniBlog.Web.Models.Blogs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abp.MiniBlog.Web.Mvc.Controllers
+{
+    [AbpMvcAuthorize(PermissionNames.Pages_Blogs)]
+    public class BlogsController : Controller
+    {
+        private readonly IBlogAppService _blogAppService;
+
+        public BlogsController(IBlogAppService blogAppService)
+        {
+            _blogAppService = blogAppService;
+        }
+
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            var allBlogs = _blogAppService.GetListAsync(null).Result;
+            return View(new BlogsListViewModel
+            {
+                Blogs = allBlogs.Items
+            });
+        }
+
+    }
+}
